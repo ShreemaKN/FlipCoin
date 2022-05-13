3c32320f825b791b4dd1c8b83b9d9f68f6cecddd
Shreema KN <shreemakn1013@gmail.com>
2022-05-14 02:39:20 +0530

    Add project files.

---
+++ BasicPrograms/Flipcoin.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BasicPrograms
+{
+    internal class Flipcoin
+    {
+        public static void Cointoss()
+        {
+            int Heads = 0;
+            int Tails = 0;
+
+            Console.WriteLine("Enter the number of times you want to flip the coin:");
+            int NumberOfflips = Convert.ToInt32(Console.ReadLine());
+
+            while (NumberOfflips <= 0)
+            {
+                Console.WriteLine("Invalid input, Please input a number greater than 0");
+                Console.WriteLine("please enter again:");
+                NumberOfflips = Convert.ToInt32(Console.ReadLine());
+            }
+
+            for (int i = 0; i < NumberOfflips; i++)
+            {
+                Random check = new Random();
+                int headortail = check.Next(0, 2);
+
+                if (headortail < 0.5)
+                {
+                    Heads++;
+                }
+                else
+                {
+                    Tails++;
+                }
+            }
+
+            float HeadsPercent = (float)Heads * 100 / NumberOfflips;
+            float TailsPercent = (float)Tails * 100 / NumberOfflips;
+
+            Console.WriteLine(HeadsPercent + "% of the times the result was heads");
+            Console.WriteLine(TailsPercent + "% of the times the result was tails");
+        }
+    }
+}
---
+++ BasicPrograms/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace BasicPrograms
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Enter a number for a particular problem");
+            Console.WriteLine("1 for Flip coin and finding percentage of heads and tails");
+
+            int Problem = Convert.ToInt32(Console.ReadLine());
+            Program basicprogram = new Program();
+            switch (Problem)
+            {
+                case 1:
+                    Flipcoin coin = new Flipcoin();
+                    Flipcoin.Cointoss();
+                    break;
+            }
+        }
+    }
+}
